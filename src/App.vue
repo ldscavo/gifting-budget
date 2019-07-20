b1507dd7d8a2491e1b2894b62c1aea688cd08675
Logan Scavo <diggerspgh@gmail.com>
2019-07-19 22:27:52 -0400

    Changed the whitespace of the main app component to be consistent with the project so far. Also added an empty method where the change saving will happen.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <Budget v-bind:budget="budgets" />
-  </div>
+    <div id="app">
+        <Budget v-bind:budget="budgets" />
+    </div>
 </template>
 
 <script>
@@ -10,31 +10,35 @@ import Recipient from './components/Recipient.vue'
 import budgetsJson from './json/budgets.json'
 
 export default {
-  name: 'app',
-  components: {
-    Budget,
-    Recipient
-  },
-  data: function() {
-    return {
-      budgets: budgetsJson 
-    }    
-  }
+    name: 'app',
+    components: {
+        Budget,
+        Recipient
+    },
+    data: function() {
+        return {
+            budgets: budgetsJson 
+        }    
+    },
+    methods: {
+        saveBudget: function() {
+
+        }
+    }
 }
 </script>
 
 <style>
 html {
-  background-color: cadetblue;
+    background-color: cadetblue;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  font-size: 1rem;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 35px;
-  
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-size: 1rem;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 35px;
 }
 </style>
