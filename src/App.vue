a96286135b708526c611f5b06fd46e4491035f66
Logan Scavo <diggerspgh@gmail.com>
2019-10-07 22:01:07 -0400

    Added basic routing to a login page and the main budget page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="app">
         <Header v-bind:budget="budget" />
-        <Budget v-bind:budget="budget" />
+        <!--<Budget v-bind:budget="budget" />-->
+        <router-view></router-view>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App.vue'
+import Budget from './components/Budget'
+import Login from './components/Login'
 import "./filters"
 
 Vue.config.productionTip = false
 
+Vue.use(Router)
+
+var router = new Router({
+  routes: [
+    { path: '/', component: Login },
+    { path: '/budget/:id', component: Budget }
+  ]
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
