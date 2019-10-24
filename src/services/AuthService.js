82185481cd61ae8c25d51e61be31f97390c67bf6
Logan Scavo <diggerspgh@gmail.com>
2019-10-23 22:41:08 -0400

    Delete AuthService.js

--- src/services/AuthService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export default {
-    register: (email, password, passwordconfirm) => axios.post('/api/register', {email, password, passwordconfirm}),
-    login: (email, password) => axios.post('/api/login', {email, password})  
-}
