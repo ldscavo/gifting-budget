a956197ab8213221ca261f3c54bd5aaa98e308c8
Logan Scavo <diggerspgh@gmail.com>
2020-03-07 20:30:13 -0500

    Made the db connection module better

--- api/db/conn.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    searchPath: ['knex', 'public'],
-};
---
+++ api/db/index.js
@@ -0,0 +1,8 @@
+let Knex = require('knex');
+let db = Knex({
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    searchPath: ['knex', 'public']
+});
+
+module.exports = db;
