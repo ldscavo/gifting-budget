dfd7fcd11064cf975776ae8fe33694bee5219979
Logan Scavo <diggerspgh@gmail.com>
2020-03-07 21:25:20 -0500

    Made items async/await

--- api/routes/items.js
+++ api/routes/items.js
@@ -1,48 +1,61 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-var knex = require('../db');
+let knex = require('../db');
 
-router.get('/budgets/:budgetId/recipients/:recipientId/items', (req, res) => {
-  knex('items').where({ recipientId: req.params.recipientId })
-    .then(items => res.json({ data: items }));
+router.get('/budgets/:budgetId/recipients/:recipientId/items', async (req, res) => {
+  let items =
+    await knex('items').where({ recipientId: req.params.recipientId });
+
+  return res.json({ data: items });
 });
 
-router.get('/budgets/:budgetId/recipients/:recipientId/items/:id', (req, res) => {
-  knex('items').where({ id: req.params.id })
-    .first()
-    .then(item => {
-      if (!item) {
-        return res.status(404).json({ error: 'item not found' })
-      }
-      return res.json({ data: item })
-    });
+router.get('/budgets/:budgetId/recipients/:recipientId/items/:id', async (req, res) => {
+  let item = 
+    await knex('items')
+      .where({ id: req.params.id })
+      .first();
+  
+  return item
+    ? res.json({ data: item })
+    : res.status(404).json({ error: 'item not found' });
 });
 
-router.post('/budgets/:budgetId/recipients/:recipientId/items', (req, res) => {
-  var item = req.body;
+router.post('/budgets/:budgetId/recipients/:recipientId/items', async (req, res) => {
+  let item = req.body;
   item.recipientId = req.params.recipientId;
+  try {
+    let newItem = 
+      await knex('items')
+        .insert(item)
+        .returning('*');
 
-  knex('items').insert(item).returning('*')
-    .then(insertedItem => res.status(201).json({ data: insertedItem[0] }))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json({ error: 'failed to create item' })
-    });
+    return res.status(201).json({ data: newItem[0] });
+  }
+   catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: 'failed to create item' });
+  }
 });
 
-router.patch('/budgets/:budgetId/recipients/:recipientId/items/:id', (req, res) => {
-  var newItemData = req.body;
-  knex('items').where({ id: req.params.id })
-    .update(newItemData)
-    .returning('*')
-    .then(data => res.json({ data: data[0] }));
+router.patch('/budgets/:budgetId/recipients/:recipientId/items/:id', async (req, res) => {
+  let newItemData = req.body;
+
+  let updatedItem =
+    await knex('items')
+      .where({ id: req.params.id })
+      .update(newItemData)
+      .returning('*');
+
+  return res.json({ data: updatedItem[0] });
 });
 
-router.delete('/budgets/:budgetId/recipients/:recipientId/items/:id', (req, res) => {
-  knex('items').where({ id: req.params.id })
-    .del()
-    .then(() => res.sendStatus(204));
+router.delete('/budgets/:budgetId/recipients/:recipientId/items/:id', async (req, res) => {
+  await knex('items')
+    .where({ id: req.params.id })
+    .del();
+    
+  return res.sendStatus(204);
 });
 
 module.exports = router;
