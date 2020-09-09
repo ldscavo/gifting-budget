53899013f90d375636e56c2d0eab4e25eb421993
Logan Scavo <logan@scavo.dev>
2020-09-08 20:23:34 -0400

    Validate the user's api key against the db

--- api/auth/validation/user.js
+++ api/auth/validation/user.js
@@ -1,5 +1,7 @@
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   var jwt = require('jsonwebtoken');
+  var knex = require('../../db');
+
   var header = req.get('Authorization') || '';
 
   var token = header.split('Bearer ');
@@ -7,12 +9,23 @@ module.exports = (req, res, next) => {
     return res.sendStatus(401);
   }
 
-  jwt.verify(token[1], process.env.PRIVATE_KEY, (err, decoded) => {
+  jwt.verify(token[1], process.env.PRIVATE_KEY, async (err, decoded) => {
     if (err) {
       return res.sendStatus(401);
     }
-
+    
     req.userId = decoded.userId;
+    
+    let savedTokens = await knex('users')
+      .where({
+        id: req.userId,
+        api_key: token[1]
+      });
+    
+    if (savedTokens.length == 0) {
+      return res.sendStatus(401);
+    }
+
     return next();
   });
 }
